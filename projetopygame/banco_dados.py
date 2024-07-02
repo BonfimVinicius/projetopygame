fd0f6f748fb1f6bdf570d7ab6f9a185322ca864d
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2024-07-02 12:53:52 -0300

    Update banco_dados.py

--- projetopygame/banco_dados.py
+++ projetopygame/banco_dados.py
@@ -35,34 +35,3 @@ def obter_posicao_ranking(nome_usuario, jogadores_ordenados):
             return idx + 1
     return None
 
-
-
-
-dados_jogo = {
-    "nome": nome_usuario,
-    "pontos": pontos,
-    "senha": senha
-}
-
-
-if novo_jogador:
-    dados_existentes["jogadores"].append(dados_jogo)
-else:
-    for jogador in dados_existentes["jogadores"]:
-        if isinstance(jogador, dict) and jogador.get("nome") == nome_usuario:
-            jogador["pontos"] = pontos
-            jogador["senha"] = senha
-
-with open(nome_arquivo, "w") as file:
-    json.dump(dados_existentes, file, indent=2)
-
-print("Dados do jogo atualizados:")
-print(json.dumps(dados_existentes, indent=2))
-
-# Atualiza os jogadores válidos e os melhores jogadores
-jogadores_validos = [jogador for jogador in dados_existentes["jogadores"] if isinstance(jogador, dict) and "pontos" in jogador]
-jogadores_ordenados = sorted(jogadores_validos, key=lambda x: x["pontos"], reverse=True)
-melhores_jogadores = jogadores_ordenados[:3]
-
-print("Top 3 jogadores:")
-print(json.dumps(melhores_jogadores, indent=2))
